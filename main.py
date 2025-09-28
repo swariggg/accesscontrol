5a9a833072861ce6c3f0a2e4b421a4d1da5f3586
swariggg <arianaremache2007@gmail.com>
2025-09-28 11:57:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 app = FastAPI()
-class loginnuser(BaseModel):
-    email:str
-    contra:str
+class LoginUser(BaseModel):
+    email: str
+    contra: str
 user_dtb={
-    "email":"arianaremache2007@gmail.com",
-    "contraseña":"AeI546"
-}
-@app.get("/login")
-def login(user:loginnuser):
-    if user==user_dtb["email"] and user.contra ==user_dtb["contraseña"]:
-        return {"mensaje":"login exitosos"}
+        "email":"arianaremache@gmail.com",
+        "contra":"AbopsJim"
+    }
+
+@app.post("/login")
+def login (user: LoginUser):
+    if user.email==user_dtb["email"] and user.contra ==user_dtb["contra"]:
+        return {"mensaje":"Login exitoso"}
     else:
-        return {"mensaje":"login fallido"}
+        return {"mensaje": "Login fallido"}
